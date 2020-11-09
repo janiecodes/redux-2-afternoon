e428f47fb5fe977bc001d032765f972b91b790cc
Janie Kim <janiekim@Janies-MBP.attlocal.net>
2020-11-09 15:30:39 -0600

    afternoon project completed

--- src/components/Budget/Budget.js
+++ src/components/Budget/Budget.js
@@ -7,24 +7,38 @@ import DisplayPurchases from './../shared/DisplayPurchases';
 import Loading from './../shared/Loading/Loading';
 import Nav from './../shared/Nav';
 import './Budget.css';
+import {connect} from 'react-redux';
+import {requestUserData} from './../../redux/userReducer'
+import {requestBudgetData, addPurchase, removePurchase} from './../../redux/budgetReducer'
+
 
 
 class Budget extends Component {
 
+  componentDidMount (){
+    this.props.requestUserData()
+    this.props.requestBudgetData()
+  }
+
   render() {
+    const {loading} = this.props.budget
+    const {purchases} = this.props.budget
+    const {budgetLimit} = this.props.budget
+
+    const {firstName, lastName} = this.props.user
     return (
       <Background>
-        {true ? <Loading /> : null}
+        {loading ? <Loading /> : null}
         <div className='budget-container'>
-          <Nav />
+          <Nav firstName={firstName} lastName={lastName}/>
           <div className='content-container'>
             <div className="purchases-container">
               <AddPurchase />
-              <DisplayPurchases />
+              <DisplayPurchases purchases={purchases}/>
             </div>
             <div className='chart-container'>
-              <Chart1 />
-              <Chart2 />
+              <Chart1 purchases={purchases} budgetLimit={budgetLimit}/>
+              <Chart2 purchases={purchases}/>
             </div>
           </div>
         </div>
@@ -32,5 +46,11 @@ class Budget extends Component {
     )
   }
 }
-
-export default Budget;
+// const mapStateToProps = state => state.budget 
+function mapStateToProps(state) {
+  return {
+    budget: state.budget,
+    user: state.user
+  }
+}
+export default connect(mapStateToProps, {requestUserData, requestBudgetData, addPurchase, removePurchase})(Budget);
